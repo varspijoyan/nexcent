1d87b0d3ce622f3856df2c0d6d3724116a606d64
varspijoyan <varsikpijoyan2003@gmail.com>
2025-03-26 20:29:11 +0400

    Add responsive styling for header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,38 @@
-import styles from "../../styles/Header.module.css";
+import Hamburger from "hamburger-react";
+import { useState } from "react";
 import logo from "../../assets/Header-Logo.svg";
+import styles from "../../styles/Header.module.css";
+
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header>
-        <div className={styles.container}>
-            <img className={styles.logo} src={logo} alt="header logo" />
-            <nav className={styles.navLinks}>
-                <ul className={styles.ulLinks}>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Features</a></li>
-                    <li><a href="#">Community</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li><a href="#">Pricing</a></li>
-                </ul>
-                <button className={styles.btn}>Register</button>
-            </nav>
-        </div>
+      <div className={styles.container}>
+        <img className={styles.logo} src={logo} alt="header logo" />
+        <nav className={styles.navLinks}>
+          <ul className={`${styles.ulLinks} ${isOpen ? styles.show : ""}`}>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Features</a>
+            </li>
+            <li>
+              <a href="#">Community</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">Pricing</a>
+            </li>
+          </ul>
+          <button className={styles.btn}>Register</button>
+          <div className={styles.respMenu}>
+            <Hamburger toggled={isOpen} toggle={setIsOpen}/>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 };
