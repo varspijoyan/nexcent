1910de92303180989d99bae6b1444698a87e1485
varspijoyan <varsikpijoyan2003@gmail.com>
2025-03-30 22:15:28 +0400

    Add english translation

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import languageDetector from "i18next-browser-languagedetector";
+import en from "./locales/en.json";
 
 i18n
 .use(initReactI18next)
@@ -10,6 +11,7 @@ i18n
     debug: true,
     resources: {
         // languages
+        en
     },
     lng: localStorage.getItem("language") || 'en'
 });
