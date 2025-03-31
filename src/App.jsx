00a7b0080758fdc18babaa1474bb0429fdc12cee
varspijoyan <varsikpijoyan2003@gmail.com>
2025-03-31 14:07:30 +0400

    Add route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+import { Route, Router } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-import MainPage from "./pages/MainPage";
 import "./i18n";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <>
-      <Header />
-      <MainPage />
-      <Footer />
+      <Router>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <MainPage />
+              <Footer />
+            </>
+          }
+        />
+      </Router>
     </>
   );
 }
