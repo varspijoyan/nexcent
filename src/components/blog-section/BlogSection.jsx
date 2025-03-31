2f1bdc0aab3fc3d6500fdc741a20515a0b1d5574
varspijoyan <varsikpijoyan2003@gmail.com>
2025-03-31 13:04:50 +0400

    Add translations in blog section

--- src/components/blog-section/BlogSection.jsx
+++ src/components/blog-section/BlogSection.jsx
@@ -1,42 +1,61 @@
-import styles from "../../styles/BlogSection.module.css";
+import { useTranslation } from "react-i18next";
 import image1 from "../../assets/BlogImage1.svg";
 import image2 from "../../assets/BlogSectionImage2.svg";
 import image3 from "../../assets/BlogSectionImage3.svg";
+import styles from "../../styles/BlogSection.module.css";
+import i18n from "../../i18n";
+
 const BlogSection = () => {
+  const { t } = useTranslation();
+  const language = i18n.language;
   return (
     <section>
       <div className={styles.container}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>Caring is the new marketing</h1>
-          <p className={styles.description}>
-            The Nextcent blog is the best place to read about the latest
-            membership insights, trends and more. See who's joining the
-            community, read about how our community are increasing their
-            membership income and lot's more.​
-          </p>
+          <h1 className={styles.title}>{t("blogSection.title")}</h1>
+          <p className={styles.description}>{t("blogSection.description")}</p>
         </div>
         <div className={styles.blogContainer}>
-            <div className={styles.blog}>
-                <img src={image1} alt="blog image" />
-                <div className={styles.blogTextContainer}>
-                    <h4 className={styles.blogTitle}>Creating Streamlined <br />Safeguarding Processes with <br />OneRed</h4>
-                    <a href="#" className={styles.link}>Read more</a>
-                </div>
+          <div className={styles.blog}>
+            <img src={image1} alt="blog image" />
+            <div className={`${styles.blogTextContainer} ${language === 'ru' ? styles.ruBlogTextContainer : ""}`}>
+              <h4 className={styles.blogTitle}>
+                {t("blogSection.blogCards.card1.titleParts.titlePart1")} <br />
+                {t("blogSection.blogCards.card1.titleParts.titlePart2")}
+                <br />
+                {t("blogSection.blogCards.card1.titleParts.titlePart3")}
+              </h4>
+              <a href="#" className={styles.link}>
+                {t("blogSection.blogCards.link")}
+              </a>
             </div>
-            <div className={styles.blog}>
-                <img src={image2} alt="blog image" />
-                <div className={styles.blogTextContainer}>
-                    <h4 className={styles.blogTitle}>What are you safeguarding <br />responsibilities and how can <br />you manage them?</h4>
-                    <a href="#" className={styles.link}>Read more</a>
-                </div>
+          </div>
+          <div className={styles.blog}>
+            <img src={image2} alt="blog image" />
+            <div className={`${styles.blogTextContainer} ${language === 'ru' ? styles.ruBlogTextContainer : ""}`}>
+              <h4 className={styles.blogTitle}>
+                {t("blogSection.blogCards.card2.titleParts.titlePart1")} <br />
+                {t("blogSection.blogCards.card2.titleParts.titlePart2")} <br />
+                {t("blogSection.blogCards.card2.titleParts.titlePart3")}?
+              </h4>
+              <a href="#" className={styles.link}>
+                {t("blogSection.blogCards.link")}
+              </a>
             </div>
-            <div className={styles.blog}>
-                <img src={image3} alt="blog image" />
-                <div className={styles.blogTextContainer}>
-                    <h4 className={styles.blogTitle}>Revamping the Membership <br />Model with Triathlon <br />Australia</h4>
-                    <a href="#" className={styles.link}>Read more</a>
-                </div>
+          </div>
+          <div className={styles.blog}>
+            <img src={image3} alt="blog image" />
+            <div className={styles.blogTextContainer}>
+              <h4 className={styles.blogTitle}>
+                {t("blogSection.blogCards.card3.titleParts.titlePart1")} <br />
+                {t("blogSection.blogCards.card3.titleParts.titlePart2")} <br />
+                {t("blogSection.blogCards.card3.titleParts.titlePart3")}
+              </h4>
+              <a href="#" className={styles.link}>
+               {t("blogSection.blogCards.link")}
+              </a>
             </div>
+          </div>
         </div>
       </div>
     </section>
