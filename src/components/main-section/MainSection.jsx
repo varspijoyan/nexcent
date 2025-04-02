e26b4ff0699bff40cf61e008e43c534da4a07ab5
varspijoyan <varsikpijoyan2003@gmail.com>
2025-04-02 17:27:53 +0400

    Add useNavigate hook to main section button to navigate to registration page

--- src/components/main-section/MainSection.jsx
+++ src/components/main-section/MainSection.jsx
@@ -1,16 +1,21 @@
 import styles from "../../styles/MainSection.module.css";
 import mainSectionImage from "../../assets/MainSectionImage.svg";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 
 const MainSection = () => {
     const { t } = useTranslation();
+    const navigate = useNavigate();
     return (
         <main>
             <div className={styles.container}>
                 <div className={styles.textContainer}>
                     <h1 className={styles.title}>{t("mainSection.titleParts.titlePart1")} <br /><span className={styles.highlight}>{t("mainSection.titleParts.titlePart2")}</span></h1>
                     <p className={styles.description}>{t("mainSection.description")}?</p>
-                    <button className={styles.btn}>{t("mainSection.button")}</button>
+                    <button 
+                        className={styles.btn}
+                        onClick={() => navigate("/registration-page")}
+                    >{t("mainSection.button")}</button>
                 </div>
                 <img className={styles.image} src={mainSectionImage} alt="main section image" />
             </div>
