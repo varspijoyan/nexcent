5694124e948f22a0aa373814be74f7ed7bd574b5
varspijoyan <varsikpijoyan2003@gmail.com>
2025-04-05 21:34:49 +0400

    Add user data fetch in use effect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/footer/Footer";
@@ -8,8 +8,22 @@ import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
 import RegistrationPage from "./pages/RegistrationPage";
 import store from "./store/store";
+import { useEffect } from "react";
+import { api } from "./services/api/api";
+import { getUser } from "./services/auth";
+import { signIn } from "./store/actions/authorizationActions";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const access = localStorage.getItem("access");
+    api.defaults.headers.common["Authorization"] = `Bearer ${access}`;
+    if(access) {
+      getUser().then(data => {
+        dispatch(signIn(data.result))
+      });
+    }
+  }, [])
   return (
     <>
       <Provider store={store}>
